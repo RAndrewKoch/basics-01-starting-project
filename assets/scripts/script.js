ee9a69b24144560c431e125fbe336117f6ef1577
Andy Koch <akandrewkoch@gmail.com>
2022-02-01 19:14:45 -0600

    first update

---
+++ assets/scripts/script.js
@@ -0,0 +1,15 @@
+const defaultResult = 0;
+
+let currentResult = defaultResult;
+
+
+currentResult = currentResult + 10 * 6;
+
+let calculationDescription = `(${defaultResult} +
+    
+    10)`;
+
+let errorMessage = 'An error \n'+' occurred!';
+
+
+outputResult(currentResult, errorMessage);
